cb717d9f5b8d899af1677f470c849be9e2d10664
alberto <a.dufour@campus.unimib.it>
2026-04-17 16:12:55 +0200

    build_cache.py for SYST directory

--- analysis/combine/build_cache.py
+++ analysis/combine/build_cache.py
@@ -42,7 +42,7 @@ SKIP_PAIRS = args.nodoubles
 MLL_BIN_EDGES = [50, 120, 200, 400, 600, 800, 1000, 3000]
 
 LHE_FILES = [
-    f"/grid_mnt/data__data.polcms/cms/adufour/MG5/mg5amcnlo/DYSMEFTMll{lo}_{hi}/Events/run_01/unweighted_events.lhe.gz"
+    f"/grid_mnt/data__data.polcms/cms/adufour/MG5/mg5amcnlo/SYST/DYSMEFTMll{lo}_{hi}/Events/run_01/unweighted_events.lhe.gz"
     for lo, hi in zip(MLL_BIN_EDGES[:-1], MLL_BIN_EDGES[1:])
 ]
 
