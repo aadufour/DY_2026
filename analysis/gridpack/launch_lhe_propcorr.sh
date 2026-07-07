1edbcc45e61ce2ff8c3fa7e82391b9c2732c5f2d
alberto <a.dufour@campus.unimib.it>
2026-07-07 11:18:59 +0200

    sbmit files parallel

---
+++ analysis/gridpack/launch_lhe_propcorr.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+export X509_USER_PROXY=/grid_mnt/data__data.polcms/cms/adufour/.t3/proxy.cert
+
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+
+tar xavf $TARBALL
+
+./runcmsgrid.sh 10000 $SEEDID 8
+
+mkdir -p $SAVEFILE/lhe_output
+mv cmsgrid_final.lhe $SAVEFILE/lhe_output/output_$SEEDID.lhe
